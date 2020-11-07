d1b8502a0009fd3ce059e763f5dbda2998fcc0d9
HerreraManuel <mherrera.jm@gmail.com>
2020-11-07 03:39:33 -0800

    loop for future cogs

--- pokeog.py
+++ pokeog.py
@@ -7,6 +7,10 @@ from database import db
 
 bot = commands.Bot(command_prefix='!')
 current_pokemon = ""
+
+# list of our cogs
+cog_collection = ['cogs.Encounter']
+
 with open('config.json') as config:
     config_data = json.load(config)
 
@@ -17,25 +21,14 @@ async def on_ready():
     # Bot Status 
     await bot.change_presence(activity=discord.Game("Pokémon Red"))
 
-@bot.command()
-async def wild(ctx):
-    # Generate random value between 1 - 151 
-    random_poke = random.randint(1, 152)
-    poke_Info = await db.wild_encounter(random_poke)
-    current_pokemon = poke_Info["name"]
-    embed=discord.Embed(description="Guess the pokémon and type !catch <pokémon> \nto catch it!", color=0x18b48d)
-    # embed.set_author(name="A wild {} has appeared!".format(poke_Info["name"]))
-    embed.set_author(name="A wild pokémon has appeared!")
-    img_loc = "res/img/{}.png".format(poke_Info["name"])
-    file=discord.File(img_loc, filename="poke.png")
-    embed.set_image(url="attachment://poke.png")
-    embed.set_footer(text="Type !help to see a list of commands")
-    await ctx.send(file=file, embed=embed)
+
 
 # catch command to check if user successfully caught a pokemon
 @bot.command()
 async def catch(ctx):
     await ctx.send("Congratulations USER! You caught a POKEMON! Added to Pokédex.")
 
-
+for single_cog in cog_collection
+    bot.load_extension(single_cog)
+    
 bot.run(config_data['token'])
