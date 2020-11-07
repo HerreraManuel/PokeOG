1a47b82c598ed7d2d5586fd0f566b67a70f4836f
HerreraManuel <mherrera.jm@gmail.com>
2020-11-07 03:31:39 -0800

    setup cogs for encounters

--- pokeog.py
+++ pokeog.py
@@ -24,7 +24,8 @@ async def wild(ctx):
     poke_Info = await db.wild_encounter(random_poke)
     current_pokemon = poke_Info["name"]
     embed=discord.Embed(description="Guess the pokémon and type !catch <pokémon> \nto catch it!", color=0x18b48d)
-    embed.set_author(name="A wild {} has appeared!".format(poke_Info["name"]))
+    # embed.set_author(name="A wild {} has appeared!".format(poke_Info["name"]))
+    embed.set_author(name="A wild pokémon has appeared!")
     img_loc = "res/img/{}.png".format(poke_Info["name"])
     file=discord.File(img_loc, filename="poke.png")
     embed.set_image(url="attachment://poke.png")
