9beeeb5b1760c358f64572e15a0d8d29f534b725
HerreraManuel <mherrera.jm@gmail.com>
2020-11-07 17:36:58 -0800

    catch command

--- cogs/encounter.py
+++ cogs/encounter.py
@@ -7,7 +7,7 @@ import os
 
 
 class Encounter(commands.Cog):
-    
+
     current_pokemon = ""
 
     def __init__(self, bot):
@@ -20,13 +20,18 @@ class Encounter(commands.Cog):
         poke_Info = await db.wild_encounter(random_poke)
         current_pokemon = poke_Info["name"]
         embed=discord.Embed(description="Guess the pokémon and type !catch <pokémon> \nto catch it!", color=0x18b48d)
-        # embed.set_author(name="A wild {} has appeared!".format(poke_Info["name"]))
         embed.set_author(name="A wild pokémon has appeared!")
-        img_loc = "res/img/{}.png".format(poke_Info["name"])
+        img_loc = "res/img/{}.png".format(current_pokemon)
         file=discord.File(img_loc, filename="poke.png")
         embed.set_image(url="attachment://poke.png")
         embed.set_footer(text="Type !help to see a list of commands")
         await ctx.send(file=file, embed=embed)
+
+    # catch command for pokemon on stage
+    @commands.command()
+    async def catch(self, ctx, arg):
+        if arg.lower() == current_pokemon:
+            await ctx.send("Congrats! you caught a pokemon")
 
 def setup(bot):
     bot.add_cog(Encounter(bot))
